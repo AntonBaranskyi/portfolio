b03ed50fbe6415805ebb7fa186ba490644e80bb7
Anton Baranskyi <antonbaranskij12@gmail.com>
2024-02-22 17:31:45 +0200

    Added social section

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -7,16 +7,21 @@ import { API } from '../../constants/API';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { toast, ToastContainer } from 'react-toastify';
+import { useForm } from 'react-hook-form';
 
 export const ContactForm = () => {
   const formRef: React.RefObject<HTMLFormElement> = useRef(null);
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: 'onBlur' });
 
+  const onSubmit = () => {
     if (formRef.current) {
       emailjs
-        .sendForm(API.emailService, API.emailTemplate, formRef?.current, {
+        .sendForm(API.emailService, API.emailTemplate, formRef.current, {
           publicKey: API.emailPublicKey,
         })
         .then(
@@ -33,17 +38,31 @@ export const ContactForm = () => {
     }
   };
 
+  console.log(errors);
+
   return (
     <div className={styles.contactForm}>
       <h3 className={styles.contactFormTitle}>Write me an email</h3>
-      <form className={styles.form} onSubmit={handleSubmit} ref={formRef}>
+      <form
+        className={styles.form}
+        onSubmit={handleSubmit(onSubmit)}
+        ref={formRef}
+      >
         <div>
           <input
             type='text'
             placeholder='Write your fullname'
             className='input'
-            name='from_name'
+            {...register('from_name', { required: true, minLength: 3 })}
           />
+          {errors.from_name?.type === 'required' && (
+            <span className={styles.formError}>This field is required</span>
+          )}
+          {errors.from_name?.type === 'minLength' && (
+            <span className={styles.formError}>
+              Minimum 3 characters required
+            </span>
+          )}
         </div>
 
         <div>
@@ -51,19 +70,35 @@ export const ContactForm = () => {
             type='email'
             placeholder='Write your email'
             className='input'
-            name='user_email'
+            {...register('user_email', {
+              required: true,
+              pattern: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+            })}
           />
+          {errors.user_email?.type === 'required' && (
+            <span className={styles.formError}>This field is required</span>
+          )}
+          {errors.user_email?.type === 'pattern' && (
+            <span className={styles.formError}>Invalid email format</span>
+          )}
         </div>
 
         <div>
           <textarea
             placeholder='Write your text'
             className={styles.formTextArea + ' input'}
-            name='message'
+            {...register('message', { required: true })}
           />
+          {errors.message?.type === 'required' && (
+            <span className={styles.formError}>This field is required</span>
+          )}
         </div>
 
-        <button type='submit' className='button'>
+        <button
+          type='submit'
+          className='button'
+          disabled={Object.entries(errors).length > 0}
+        >
           Send
         </button>
       </form>
--- src/components/ContactSocial/ContactSocial.tsx
+++ src/components/ContactSocial/ContactSocial.tsx
@@ -1,9 +1,49 @@
 import styles from './ContactSocial.module.scss';
 
+import telegram from '../../assets/images/telegram.png';
+import linkedin from '../../assets/images/linkedin.png';
+import gitHub from '../../assets/images/gitHub.png';
+
+export const socialData = [
+  {
+    image: telegram,
+    nickname: '@baranskyi_03',
+    link: 'https://t.me/baranskyi_03',
+  },
+  {
+    image: linkedin,
+    nickname: 'Anton Baranskyi',
+    link: 'https://www.linkedin.com/in/anton-baranskyi-407836283/',
+  },
+  {
+    image: gitHub,
+    nickname: 'Anton Baranskyi',
+    link: 'https://github.com/AntonBaranskyi',
+  },
+];
+
 export const ContactSocial = () => {
   return (
     <div className={styles.contactSocial}>
       <h3 className={styles.contactSocialTitle}>Or find me in social media</h3>
+
+      <div className={styles.contactSocialWrapper}>
+        {socialData.map((social) => (
+          <a
+            key={social.link}
+            className={styles.contactSocialItem}
+            href={social.link}
+            target='_blank'
+          >
+            <img
+              src={social.image}
+              alt='image'
+              className={styles.contactSocialImage}
+            />
+            <p className={styles.contactSocialNickName}>{social.nickname}</p>
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
